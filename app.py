af01496725c04c65dcf8f6c709d24c5730751bb2
unknown <nannicque@gmail.com>
2024-06-15 01:38:20 +0200

    bonjour

--- app.py
+++ app.py
@@ -82,11 +82,13 @@ def calculate():
 
         Xa_values = np.linspace(0, 0.7, 100)  # Fraction molaire de A
 
+        debut = time.time()
         # Fonction pour calculer D_AB à partir des coefficients a_AB et a_BA
         def calculate_DAB(Xa):
             a12 = 236.82636803699774
             a21 = 1268.1042421722684
             Xb = 1 - Xa  # Fraction molaire de B
+            
             # Calcul des termes de l'équation
             term1 = Xb*np.log(D0AB) + Xa*D0BA + 2*(Xa*np.log((Xa*r_a+Xb*r_b)/r_a)+Xb*np.log((Xb*r_b+Xa*r_a)/r_b))
             term2 = 2*Xa*Xb*((r_a/(Xa*r_a+Xb*r_b))*(1-(r_a/r_b)) + (r_b/(Xa*r_a+Xb*r_b))*(1-(r_b/r_a)))
@@ -98,6 +100,9 @@ def calculate():
 
         # Calcul de D_AB avec les paramètres optimisés
         D_AB_values = [calculate_DAB(Xa) for Xa in Xa_values]
+
+        fin = time.time()
+        temps = fin-debut 
 
         # Tracer la variation du coefficient de diffusion en fonction de la fraction molaire
         plt.plot(Xa_values, D_AB_values)
@@ -116,5 +121,5 @@ def calculate():
         time.sleep(3)
 
         # Renvoyer le modèle output.html avec les résultats calculés
-    return render_template('output.html', a12=a12_opt, a21=a21_opt, error=error, D_AB=D_AB_opt, plot_encoded=plot_encoded)
+    return render_template('output.html', a12=a12_opt, a21=a21_opt, error=error, D_AB=D_AB_opt, plot_encoded=plot_encoded,iteration=iteration,temps=temps)
 
